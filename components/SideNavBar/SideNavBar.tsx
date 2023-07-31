a1622fc3781949c26f1ff4b965ccbf875ed700dd
ahmadsalehuddin <ahmadsalehuddinazzizi@gmail.com>
2023-07-31 12:19:41 +0800

    feat: add side navigation bar; remove main heading

--- components/SideNavBar/SideNavBar.tsx
+++ components/SideNavBar/SideNavBar.tsx
@@ -3,71 +3,43 @@ import { faBolt, faBucket } from "@fortawesome/free-solid-svg-icons";
 
 export default function SideNavBar() {
   return (
-    <div className="">
-      <div className="border rounded mb-2 text-center">
-        <h1 className="fs-2 m-4">Web3 Browser</h1>
+    <>
+      <div className="border border-2 rounded border-dark px-4 py-3 mb-2">
+        <p className="fs-4 fw-medium m-0 mb-1">Quick Access</p>
+        <table className="table table-borderless table-hover m-0">
+          <tbody>
+            <tr>
+              <td>Photo.jpg</td>
+            </tr>
+            <tr>
+              <td>Video.mp4</td>
+            </tr>
+            <tr>
+              <td>Document.docx</td>
+            </tr>
+          </tbody>
+        </table>
       </div>
 
-      <div className="border rounded mb-2 p-3">
-        <FontAwesomeIcon icon={faBolt} />
-        Quick Access
-        <ul className="nav flex-column ms-3">
-          <li className="nav-item">
-            <a className="nav-link" href="#">
-              Photo.jpg
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">
-              Video.mp4
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">
-              Document.docx
-            </a>
-          </li>
-        </ul>
-        <hr />
-        <FontAwesomeIcon icon={faBucket} />
-        Browse Bucket
-        <ul className="nav flex-column ms-3">
-          <li className="nav-item">
-            <a className="nav-link" href="#">
-              Bucket A
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">
-              Bucket B
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">
-              Bucket C
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">
-              Show all
-            </a>
-          </li>
-        </ul>
+      <div className="border border-2 rounded border-dark px-4 py-3">
+        <p className="fs-4 fw-medium m-0 mb-1">Browse Bucket</p>
+        <table className="table table-borderless table-hover m-0">
+          <tbody>
+            <tr>
+              <td>Bucket A</td>
+            </tr>
+            <tr>
+              <td>Bucket B</td>
+            </tr>
+            <tr>
+              <td>Bucket C</td>
+            </tr>
+            <tr>
+              <td>Show all</td>
+            </tr>
+          </tbody>
+        </table>
       </div>
-
-      <div className="border rounded">
-        <nav>
-          <ul>
-            <li>
-              <a href="#">Guide</a>
-            </li>
-            <li>
-              <a href="#">About</a>
-            </li>
-          </ul>
-          V1.0.0
-        </nav>
-      </div>
-    </div>
+    </>
   );
 }
