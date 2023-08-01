7c8eb9592ee3336524c10d90ef61a15bedef14dc
ahmadsalehuddin <ahmadsalehuddinazzizi@gmail.com>
2023-08-01 09:04:17 +0800

    feat: add heading card; resize border; add icon

--- components/SideNavBar/SideNavBar.tsx
+++ components/SideNavBar/SideNavBar.tsx
@@ -1,15 +1,31 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBolt, faBucket } from "@fortawesome/free-solid-svg-icons";
+import {
+  faStar as faStarRegular,
+  faImage as faImageRegular,
+} from "@fortawesome/free-regular-svg-icons";
+import {
+  faBolt,
+  faBucket,
+  faStar as faStarSolid,
+} from "@fortawesome/free-solid-svg-icons";
 
 export default function SideNavBar() {
   return (
     <>
-      <div className="border border-2 rounded border-dark px-4 py-3 mb-2">
-        <p className="fs-4 fw-medium m-0 mb-1">Quick Access</p>
+      <div className="border border-3 rounded border-dark py-4 mb-2">
+        <h1 className="m-0 fs-2 text-center">Web3 Browser</h1>
+      </div>
+
+      <div className="border border-3 rounded border-dark px-4 py-4 mb-2">
+        <p className="fs-4 fw-medium m-0 mb-2">Quick Access</p>
         <table className="table table-borderless table-hover m-0">
           <tbody>
             <tr>
-              <td>Photo.jpg</td>
+              <td className="d-flex align-items-center">
+                <FontAwesomeIcon icon={faImageRegular} className="fs-5" />{" "}
+                Photo.jpg
+                <FontAwesomeIcon icon={faStarRegular} className="ms-auto fs-5" />
+              </td>
             </tr>
             <tr>
               <td>Video.mp4</td>
@@ -21,8 +37,8 @@ export default function SideNavBar() {
         </table>
       </div>
 
-      <div className="border border-2 rounded border-dark px-4 py-3">
-        <p className="fs-4 fw-medium m-0 mb-1">Browse Bucket</p>
+      <div className="border border-3 rounded border-dark px-4 py-4">
+        <p className="fs-4 fw-medium m-0 mb-2">Browse Bucket</p>
         <table className="table table-borderless table-hover m-0">
           <tbody>
             <tr>
