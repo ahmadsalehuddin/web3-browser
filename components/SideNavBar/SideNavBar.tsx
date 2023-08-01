d3dd233950cf60521aa43b395771a747191d578e
ahmadsalehuddin <ahmadsalehuddinazzizi@gmail.com>
2023-08-01 19:17:09 +0800

    feat: add icons; adjust font size; style quick access and bucket list items

--- components/SideNavBar/SideNavBar.tsx
+++ components/SideNavBar/SideNavBar.tsx
@@ -1,11 +1,10 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStar as faStarRegular } from "@fortawesome/free-regular-svg-icons";
 import {
-  faStar as faStarRegular,
-  faImage as faImageRegular,
-} from "@fortawesome/free-regular-svg-icons";
-import {
-  faBolt,
   faBucket,
+  faFileWord,
+  faImage,
+  faVideo,
   faStar as faStarSolid,
 } from "@fortawesome/free-solid-svg-icons";
 
@@ -13,42 +12,89 @@ export default function SideNavBar() {
   return (
     <>
       <div className="border border-3 rounded border-dark py-4 mb-2">
-        <h1 className="m-0 fs-2 text-center">Web3 Browser</h1>
+        <h1 className="m-0 fs-3 text-center">Web3 Browser</h1>
       </div>
 
       <div className="border border-3 rounded border-dark px-4 py-4 mb-2">
-        <p className="fs-4 fw-medium m-0 mb-2">Quick Access</p>
+        <p className="fs-5 fw-medium m-0 mb-2">Quick Access</p>
         <table className="table table-borderless table-hover m-0">
           <tbody>
             <tr>
               <td className="d-flex align-items-center">
-                <FontAwesomeIcon icon={faImageRegular} className="fs-5" />{" "}
+                <FontAwesomeIcon
+                  icon={faImage}
+                  className="me-3 fs-6"
+                  style={{ minWidth: "18px" }}
+                />{" "}
                 Photo.jpg
-                <FontAwesomeIcon icon={faStarRegular} className="ms-auto fs-5" />
+                <FontAwesomeIcon
+                  icon={faStarRegular}
+                  className="ms-auto fs-6"
+                />
               </td>
             </tr>
             <tr>
-              <td>Video.mp4</td>
+              <td className="d-flex align-items-center">
+                <FontAwesomeIcon
+                  icon={faVideo}
+                  className="me-3 fs-6"
+                  style={{ minWidth: "18px" }}
+                />{" "}
+                Video.mp4
+                <FontAwesomeIcon icon={faStarSolid} className="ms-auto fs-6" />
+              </td>
             </tr>
             <tr>
-              <td>Document.docx</td>
+              <td className="d-flex align-items-center">
+                <FontAwesomeIcon
+                  icon={faFileWord}
+                  className="me-3 fs-6"
+                  style={{ minWidth: "18px" }}
+                />{" "}
+                Document.docx
+                <FontAwesomeIcon
+                  icon={faStarRegular}
+                  className="ms-auto fs-6"
+                />
+              </td>
             </tr>
           </tbody>
         </table>
       </div>
 
       <div className="border border-3 rounded border-dark px-4 py-4">
-        <p className="fs-4 fw-medium m-0 mb-2">Browse Bucket</p>
+        <p className="fs-5 fw-medium m-0 mb-2">Browse Bucket</p>
         <table className="table table-borderless table-hover m-0">
           <tbody>
             <tr>
-              <td>Bucket A</td>
+              <td className="d-flex align-items-center">
+                <FontAwesomeIcon
+                  icon={faBucket}
+                  className="me-3 fs-6"
+                  style={{ minWidth: "18px" }}
+                />{" "}
+                Bucket A
+              </td>
             </tr>
             <tr>
-              <td>Bucket B</td>
+              <td className="d-flex align-items-center">
+                <FontAwesomeIcon
+                  icon={faBucket}
+                  className="me-3 fs-6"
+                  style={{ minWidth: "18px" }}
+                />{" "}
+                Bucket B
+              </td>
             </tr>
             <tr>
-              <td>Bucket C</td>
+              <td className="d-flex align-items-center">
+                <FontAwesomeIcon
+                  icon={faBucket}
+                  className="me-3 fs-6"
+                  style={{ minWidth: "18px" }}
+                />{" "}
+                Bucket C
+              </td>
             </tr>
             <tr>
               <td>Show all</td>
